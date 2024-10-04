027863274474a9371ad9256804be66452ccf963c
Gnanasree Athimuru <gnanasreeathimuru@gmail.com>
2024-10-04 20:28:49 +0530

    Java programs

---
+++ src/Oct/Ex_03102024/Lab003.java
@@ -0,0 +1,16 @@
+package Oct.Ex_03102024;
+
+public class Lab003 {
+    //simple calculator using SOP statements
+
+        public static void main (String[] args)
+        {
+            int a=20;
+            int b=5;
+            System.out.println(a+b);
+            System.out.println(a-b);
+            System.out.println(a/b);
+            System.out.println(a%b);
+        }
+    }
+
