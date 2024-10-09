92312ae386ba5bcabaed84336245709f07127974
Gnanasree Athimuru <gnanasreeathimuru@gmail.com>
2024-10-09 20:52:55 +0530

    Java programs1

---
+++ src/Oct/Ex_09102024/PrintingTable.java
@@ -0,0 +1,26 @@
+package Oct.Ex_09102024;
+
+public class PrintingTable {
+public static void main(String[]args){
+    int a=15;
+    System.out.printf("%d*%d=%d",a,1,a);
+    System.out.println();
+    System.out.printf("%d*%d=%d",a,1,a*2);
+    System.out.println();
+    System.out.printf("%d*%d=%d",a,1,a*3);
+    System.out.println();
+    System.out.printf("%d*%d=%d",a,1,a*4);
+    System.out.println();
+    System.out.printf("%d*%d=%d",a,1,a*5);
+    System.out.println();
+    System.out.printf("%d*%d=%d",a,1,a*6);
+    System.out.println();
+    System.out.printf("%d*%d=%d",a,1,a*7);
+    System.out.println();
+    System.out.printf("%d*%d=%d",a,1,a*8);
+    System.out.println();
+    System.out.printf("%d*%d=%d",a,1,a*9);
+    System.out.println();
+    System.out.printf("%d*%d=%d",a,1,a*10);
+}
+}
